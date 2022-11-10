c20e704fd697910d0516b0530647e3e9d93f815c
Jack Ma <guoyaoma66@gmail.com>
2022-11-10 18:09:53 -0500

    Page margin 2.5rem

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import * as React from 'react';
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-export default MyApp
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          margin: '2.5rem',
+        },
+      },
+    },
+  },
+});
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
